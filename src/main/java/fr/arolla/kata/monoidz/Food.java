cd77c4025d7e3c6c180979a1a1ea616050492e39
Rachid OUHSSAINE <rachid.ouhssaine@outlook.com>
2022-08-06 13:40:12 +0200

    [MONOIDZ-KATA] is organic food ?

--- src/main/java/fr/arolla/kata/monoidz/Food.java
+++ src/main/java/fr/arolla/kata/monoidz/Food.java
@@ -1,10 +1,11 @@
 package fr.arolla.kata.monoidz;
 
-public record Food(Diet diet) {
+public record Food(Diet diet, boolean isOrganic) {
 
     public Food combine(Food food) {
         Diet combineDiet = this.diet.combine(food.diet);
-        return new Food(combineDiet);
+        boolean isOrganic = food.isOrganic && this.isOrganic;
+        return new Food(combineDiet, isOrganic);
     }
 
 }
--- src/test/java/fr/arolla/kata/monoidz/FoodTest.java
+++ src/test/java/fr/arolla/kata/monoidz/FoodTest.java
@@ -14,13 +14,28 @@ public class FoodTest {
     @Test
     void should_return_vegetarian_food_when_combine_vegan_and_vegetarian_food() {
         // Given
-        Food salad = new Food(VEGAN);
-        Food cheese = new Food(VEGETARIAN);
+        Food salad = new Food(VEGAN, true);
+        Food cheese = new Food(VEGETARIAN, true);
 
         // When
         Food result = salad.combine(cheese);
 
         // Then
         assertThat(result.diet()).isEqualTo(VEGETARIAN);
+        assertThat(result.isOrganic()).isTrue();
+    }
+
+    @Test
+    void should_return_not_organic_foor_when_has_not_organic_food_in_composition() {
+        // Given
+        Food salad = new Food(VEGAN, false);
+        Food cheese = new Food(VEGAN, true);
+
+        // When
+        Food result = salad.combine(cheese);
+
+        // Then
+        assertThat(result.diet()).isEqualTo(VEGAN);
+        assertThat(result.isOrganic()).isFalse();
     }
 }
